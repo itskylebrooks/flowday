d560d106d3763e4c66a2564a23f68b820fc31e54
h-off-nung <consist-canteen8e@icloud.com>
2025-08-22 16:13:49 +0200

    feat(config): add server configuration for allowed hosts in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['40a0ccc880f0.ngrok-free.app'],
+  },
 })
